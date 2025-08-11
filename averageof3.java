0198d8ce1e530492a360405168882d9c1571686a
MD-NABEEL-T <tmdnabeel4656.tmn@gmail.com>
2025-08-11 15:00:31 +0530

    solved problem 1

---
+++ averageof3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class averageof3
+{
+    public static int avg(int num1,int num2,int num3)
+    {
+        int averageValue = (num1+num2+num3)/3;
+        return averageValue;
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1");
+        int num1 = sc.nextInt();
+        System.out.println("Enter number 2");
+        int num2 = sc.nextInt();
+        System.out.println("Enter number 3");
+        int num3 = sc.nextInt();
+
+        int average = avg(num1,num2,num3);
+        System.out.println(average);
+    }
+}
